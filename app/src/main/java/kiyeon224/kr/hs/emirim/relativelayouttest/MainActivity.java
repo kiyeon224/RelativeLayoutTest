c17030aacd303a378a0eca57c4a31e0d8a308182
kiyeon224 <kiyeon224@naver.com>
2017-04-04 19:27:36 +0900

    #1 RelativeLayout Base Widget

---
+++ app/src/main/java/kiyeon224/kr/hs/emirim/relativelayouttest/MainActivity.java
@@ -0,0 +1,13 @@
+package kiyeon224.kr.hs.emirim.relativelayouttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  //activity_main2하면 2를 볼 수 있음
+    }
+}
